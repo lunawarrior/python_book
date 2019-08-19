56ed2889b4c57f6bf646e6f5f04e0eb7d4afed37
RLProGamer <iloveapex8501@users.noreply.github.com>
2019-08-19 17:30:40 -0400

    Update on Odd Numbers

--- Exercises/7/1_odd_numbers.py
+++ Exercises/7/1_odd_numbers.py
@@ -6,11 +6,12 @@ Write a function to count how many odd numbers are in a list.
 from test import test
 
 #Define your count_odd function here
-
+def count_odd(numbers):
+    if(numbers == )
 
 # Here are the tests
 test(count_odd([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) == 5)
 test(count_odd([1, -1, 1, 1]) == 4)
-test(count_odd(2, 4, -6, 8) == 0)
+test(count_odd([2, 4, -6, 8]) == 0)
 
 
