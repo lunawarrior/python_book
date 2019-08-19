6da2c14d0935bb57c86890a6e54cf14464ba5edc
Laptop <kkirke@gmail.com>
2019-08-19 17:26:43 -0400

    Added exercise 8.2

---
+++ Exercises/8/2_duck_quack.py
@@ -0,0 +1,14 @@
+
+'''
+Modify the following code
+so that it produces the output on the following two lines:
+Ouack
+Quack
+'''
+
+prefixes = "JKLMNOPQ"
+suffix = "ack"
+
+for letter in prefixes:
+    print(letter + suffix)
+
