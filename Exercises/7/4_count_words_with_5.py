6bf72a1c9176d5ad2c081db99bc980d8b6865d17
RLProGamer <iloveapex8501@users.noreply.github.com>
2019-08-26 17:25:40 -0400

    Done with count+words_with_5. WHY????

--- Exercises/7/4_count_words_with_5.py
+++ Exercises/7/4_count_words_with_5.py
@@ -6,7 +6,13 @@ Write a function to count how many words in a list have length 5.
 from test import test
 
 #Define your count_5 function here
-
+def count_5(the_list):
+    count = 0
+    for item in the_list:
+        if len (item) == 5:
+            count = count + 1
+    return count
+    
 
 # Here are the tests
 test(count_5(['a', 'word', 'abcde', 'exact', 'five']), 2)
