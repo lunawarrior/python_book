78f5fd37bbe2b6a76a6ce5e544570c213cccf773
RLProGamer <iloveapex8501@users.noreply.github.com>
2019-09-13 17:54:14 -0400

    Completed! I beg for mercy!!

--- Exercises/8/8_mirror_string.py
+++ Exercises/8/8_mirror_string.py
@@ -5,8 +5,16 @@ Write a function that mirrors its argument
 '''
 from test import test
 
+def reverse(the_string_start):
+    the_reversed_string = ''
+    for char in the_string_start:
+        the_reversed_string = char + the_reversed_string
+    return the_reversed_string
 
-
+def mirror(the_string):
+    mirror_string = reverse(the_string)
+    mirror_string = the_string + mirror_string
+    return mirror_string
 
 
 
--- Exercises/8/9_remove_a_letter.py
+++ Exercises/8/9_remove_a_letter.py
@@ -5,12 +5,20 @@ Write a function that removes all occurrences of a given letter from a string
 '''
 from test import test
 
+def remove_letter(letter, word):
+    # character
+    the_string = ''
+    for char in word:
+        if char != letter:
+            the_string = the_string + char
+    return the_string
+        
+    
 
 
 
 
-
-
+test(remove_letter("p", "happy"), "hay")
 test(remove_letter("a", "apple"), "pple")
 test(remove_letter("a", "banana"), "bnn")
 test(remove_letter("z", "banana"), "banana")
