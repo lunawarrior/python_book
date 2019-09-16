075dd36e23f3ba3cd20f8a8ea3b9c7ac6cca7ed2
TruDireWolfy <54290193+TruDireWolfy@users.noreply.github.com>
2019-09-16 17:54:24 -0400

    yrfy
    nuh

--- Exercises/8/7_reverse_string.py
+++ Exercises/8/7_reverse_string.py
@@ -5,6 +5,16 @@ Write a function that reverses its string argument, and satisfies these tests:
 
 '''
 from test import test
+def reverse(the_str):
+    the_reserve_string = ""
+    for letter in the_str:
+        the_reserve_string = letter + the_reserve_string
+        # print(the_reserve_string)
+
+    return the_reserve_string
+    
+
+
 
 
 
--- Exercises/8/8_mirror_string.py
+++ Exercises/8/8_mirror_string.py
@@ -4,7 +4,19 @@ Write a function that mirrors its argument
 
 '''
 from test import test
+def reverse(the_str):
+    the_reverse_string = ""
+    for letter in the_str:
+        the_reverse_string = letter + the_reverse_string
+        # print(the_reserve_string)
 
+    return the_reverse_string
+
+def mirror(the_word):
+    the_reverse_string = reverse(the_word)
+    the_word = the_word + the_reverse_string
+
+    return the_word
 
 
 
