781c3bf179ac96a8f1ae354501f078955f4d4beb
Laptop <kkirke@gmail.com>
2019-10-04 17:54:09 -0400

    Create 0_learning_lists.py

---
+++ Exercises/11_lists/0_learning_lists.py
@@ -0,0 +1,49 @@
+# # Items in Lists
+# students = [
+#     ("John", ["CompSci", "Physics"]),
+#     ("Vusi", ["Maths", "CompSci", "Stats"]),
+#     ("Jess", ["CompSci", "Accounting", "Economics", "Management"]),
+#     ("Sarah", ["InfSys", "Accounting", "Economics", "CommLaw"]),
+#     ("Zuki", ["Sociology", "Economics", "Law", "Stats", "Music"])]
+
+# # Count how many students are taking CompSci
+# counter = 0
+# for (name, subjects) in students:
+#     if "CompSci" in subjects:
+#            counter += 1
+#         #  counter = counter + 1
+
+# print("The number of students taking CompSci is", counter)
+
+# # concatenation
+# a = [1, 2, 3]
+# b = [4, 5, 6]
+# c = a + b
+# print(c)
+
+# # Slices
+# c = [0, 1, 2, 3, 4, 5, 6]
+# print( c[3:5] )
+
+# #Lists are mutable
+# fruit = ["banana", "apple", "quince"]
+# # print(fruit[1])
+# fruit[1] = "strawaberry"
+# print(fruit)
+
+# # You can replace slices in a list
+# a_list = ["a", "b", "c", "d", "e", "f"]
+# # print(a_list[1:3])
+# a_list[1:3] = ["x", "y"]
+# print(a_list)
+
+# # And remove them
+# a_list[1:3] = []
+# print(a_list)
+
+# # And add them
+# a_list[1:1] = ["b", "c"]
+# print(a_list)
+
+
+#At 11.9
