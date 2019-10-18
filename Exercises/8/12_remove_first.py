5b759015f3177cb57cfdaa6a1df3e3f53d829edc
TruDireWolfy <54290193+TruDireWolfy@users.noreply.github.com>
2019-10-18 16:46:49 -0400

    Update 12_remove_first.py

--- Exercises/8/12_remove_first.py
+++ Exercises/8/12_remove_first.py
@@ -6,7 +6,19 @@ Write a function that removes the first occurrence of a string from another stri
 from test import test
 
 
+def remove(part,word):
+    #print(word.find(part))
+    #print(word.split('a'))
+    index = word.find(part)
+    #print(word[:index])
 
+    #print(word[index + len(part):]
+    print(index)
+    if index > -1:
+        return word[:index] + word[index + len(part):]
+    else:
+        return word
+        
 
 
 test(remove("an", "banana"), "bana")
