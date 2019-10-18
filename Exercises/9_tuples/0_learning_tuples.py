d8afa5ff5655d6427354f7c5017eb375e2c7869b
Laptop <kkirke@gmail.com>
2019-10-18 16:22:32 -0400

    Update 0_learning_tuples.py

--- Exercises/9_tuples/0_learning_tuples.py
+++ Exercises/9_tuples/0_learning_tuples.py
@@ -1,27 +1,27 @@
 julia = ("Julia", "Roberts", 1967, "Duplicity", 2009, "Actress", "Atlanta, Georgia")
 
-# print(julia)
+# # print(julia)
 
-(name, surname, b_year, movie, m_year, profession, b_place) = julia
+# (name, surname, b_year, movie, m_year, profession, b_place) = julia
 
-def divide_with_remainder(top, bottom):
-    result = top // bottom
-    remainder = top % bottom
-    return (result, remainder)
+# def divide_with_remainder(top, bottom):
+#     result = top // bottom
+#     remainder = top % bottom
+#     return (result, remainder)
 
-a, b = divide_with_remainder(13, 2)
-# print(a)
-# print(b)
+# a, b = divide_with_remainder(13, 2)
+# # print(a)
+# # print(b)
 
-#Swap a and b
-(b, a) = (a, b)
+# #Swap a and b
+# (b, a) = (a, b)
 
 
 
-student = ("Stewart", ("Math", "Biology", "calculus"), "a third item")
+# student = ("Stewart", ("Math", "Biology", "calculus"), "a third item")
 
-classes = student[1]
-bio = classes[1]
+# classes = student[1]
+# bio = classes[1]
 
-#Print biology from student
-print(student[1][1])
+# #Print biology from student
+# print(student[1][1])
