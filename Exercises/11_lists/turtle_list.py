e7c1dd0764d1eece2dbbb831929f24321737a611
Laptop <kkirke@gmail.com>
2019-11-18 16:07:08 -0500

    Create turtle_list.py

---
+++ Exercises/11_lists/turtle_list.py
@@ -0,0 +1,19 @@
+import turtle             # Allows us to use turtles
+
+alex = turtle.Turtle() 
+wn = turtle.Screen()
+
+alex.forward(50)
+alex.left(90)   
+
+alex.forward(50)   
+alex.left(90)
+
+alex.forward(50)
+alex.left(90)
+
+alex.forward(50)
+alex.left(90)
+
+wn.mainloop()
+
