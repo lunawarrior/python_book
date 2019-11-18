d81304fefe4390f59771e417d614a105a1eacbd5
Laptop <kkirke@gmail.com>
2019-11-18 16:10:13 -0500

    Update turtle_list.py

--- Exercises/11_lists/turtle_list.py
+++ Exercises/11_lists/turtle_list.py
@@ -3,6 +3,8 @@ import turtle             # Allows us to use turtles
 alex = turtle.Turtle() 
 wn = turtle.Screen()
 
+colors = ['green', 'blue', 'yellow', 'red', 'black', 'teal']
+
 alex.forward(50)
 alex.left(90)   
 
