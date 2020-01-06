64b513aac9c92937344f1d86dae63a8a06f95344
Laptop <kkirke@gmail.com>
2020-01-06 15:56:21 -0500

    stuff

---
+++ Exercises/15_classes_and_objects_basics/0_start.py
@@ -0,0 +1,9 @@
+# Before this we were using "Procedural Programming".  
+# Focuses is on writing functions or procedures which operate on data.
+
+# Now we will be starting "Object Oriented Programming". 
+# is on the creation of objects which contain both data and functionality together. 
+# (We have seen turtle objects, string objects, and random number generators)
+
+# See "point.py" for an example of a class with an "initializer method"
+
---
+++ Exercises/15_classes_and_objects_basics/point.py
@@ -0,0 +1,7 @@
+class Point:
+    """ Point class represents and manipulates x,y coords. """
+
+    def __init__(self):
+        """ Create a new point at the origin """
+        self.x = 0
+        self.y = 0
