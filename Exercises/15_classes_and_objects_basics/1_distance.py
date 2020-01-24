54d89fe93755f77b201f0dbbfb6a3e6b3f65d84a
RLProGamer <iloveapex8501@users.noreply.github.com>
2020-01-24 16:24:24 -0500

    hi

--- Exercises/15_classes_and_objects_basics/1_distance.py
+++ Exercises/15_classes_and_objects_basics/1_distance.py
@@ -8,4 +8,17 @@ def distance(x1, y1, x2, y2):
     dsquared = dx*dx + dy*dy
     result = dsquared**0.5
     return result
+def distance(new_point, last_point):
+    dx = new_point.x - last_point.x
+    dy = new_point.y - last_point.y
+    return ((dx ** 2) + (dy ** 2)) ** .5
 
+    
+
+p = Point(5, 5)
+p2 = Point(2, 1)
+
+print(distance(p, p2))
+
+
+# print(distance(5, 5, 2, 1))
